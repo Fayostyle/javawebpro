8d110156293042bc5cbcc09f8a646afc4f79bf3e
fayostyle <phuang5945@163.com>
2017-06-09 21:23:39 +0800

    增加BaseServlet基础类， 其他servlet都继承自BaseServlet 增加WebUtils工具类

---
+++ javawebpro/day2_restaurant/src/cn/fayostyle/servlet/BaseServlet.java
@@ -0,0 +1,52 @@
+package cn.fayostyle.servlet;
+
+import cn.fayostyle.factory.BeanFactory;
+import cn.fayostyle.service.IDinnerTableService;
+import cn.fayostyle.service.IFoodTypeService;
+import cn.fayostyle.utils.WebUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ *  项目中通用的servlet， 希望所有的servlet都继承此类
+ * Created by HuangPan on 2017/6/9.
+ */
+public class BaseServlet extends HttpServlet {
+
+    protected IFoodTypeService foodTypeService = BeanFactory.getInstance("foodTypeService", IFoodTypeService.class);
+    protected IDinnerTableService dinnerTableService = BeanFactory.getInstance("dinnerTableService", IDinnerTableService.class);
+
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //（保存跳转的资源） 方法返回值
+        Object returnValue = null;
+
+        //获取操作类型
+        String methodName = request.getParameter("method");
+
+        try {
+            //获取当前运行类的字节码
+            Class clazz = this.getClass();
+            //获取当前执行方法的Method类型
+            Method method = clazz.getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            //执行方法
+            returnValue = method.invoke(this, request, response);
+        } catch (Exception e) {
+            returnValue = "/error/error.jsp";
+        }
+
+        //跳转
+        WebUtils.goTo(request, response, returnValue);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
---
+++ javawebpro/day2_restaurant/src/cn/fayostyle/utils/WebUtils.java
@@ -0,0 +1,23 @@
+package cn.fayostyle.utils;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by HuangPan on 2017/6/9.
+ */
+public class WebUtils {
+    /**
+     * 跳转的通用方法
+     */
+    public static void goTo(HttpServletRequest request, HttpServletResponse response, Object uri) throws ServletException, IOException {
+        if(uri instanceof RequestDispatcher) {
+            ((RequestDispatcher) uri).forward(request, response);
+        } else if (uri instanceof String) {
+            response.sendRedirect(request.getContextPath() + uri);
+        }
+    }
+}
